5046cce93e885b3517e5ec1e992aa1a392852bc5
Swarup Vijay Pokharkar <110724501+swarup-2004@users.noreply.github.com>
2024-01-28 00:46:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,56 +1,58 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import tensorflow as tf 
 from tensorflow import keras
 
 
-
-
-#function to predict class of the image
+# Function to predict class of the image
 def predict_class(probab):
     num = np.argmax(probab)
     classes = ['Arjun', 'Bramhi', 'Curry', 'Mint', 'Neem', 'Rubble']
     return classes[num]
 
-#app title and description
-st.title("Medicinal Plant Classifier")
+# App title and description
+st.title("Plant Identifier")
 st.write("Upload an image of the plant.")
 
-#upload image through file uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# User option to upload image or capture from camera
+upload_option = st.radio("Choose an option:", ["Upload Image", "Capture from Camera"])
 
-#display the uploaded image
+uploaded_file = None
+
+if upload_option == "Upload Image":
+    # Upload image through file uploader
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    
+
+elif upload_option == "Capture from Camera":
+    # Capture image from the camera
+    uploaded_file = st.camera_input("Take a picture")
+        
+
+# Display the uploaded or captured image
 if uploaded_file is not None:
-    #read and display the image
+    # Display the image with a caption
     image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded/Captured Image", use_column_width=True)
 
-    #display the image with a caption
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    #preprocessing the image for the model to predict
+    # Preprocessing the image for the model to predict
     img = image.resize((128, 128))
-    img = np.array(img)
+    img = np.array(img) / 255.0
 
-    #add an extra dimension to simulate a batch of size 1
+    # Add an extra dimension to simulate a batch of size 1
     img = np.expand_dims(img, axis=0)
 
     ok = st.button("Predict")
 
-    #loadig the neural model
+    # Load the neural model
     model = keras.models.load_model("my_keras_model.h5")
 
     if ok:
-
         
 
-        #predicting the class of the model
+        # Predicting the class of the model
         predictions = model.predict(img)
 
-
-        #showing predicted output
+        # Showing predicted output
         st.write("Image class:", predict_class(predictions))
-
-
-
-    
